0a32313e8b953dd4e939446356ee9aa28a8182fb
Sviatoslav <sviatoslav29041967@gmail.com>
2020-06-29 17:02:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// "comment me"
+console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(4/0); 
+console.log("string" * 12);
+let something;
+console.log(something);
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+}
+console.log(persone.name);
+console.log(persone['name']);
+let arr = ['plumb.jpg','orange.png','apple.bmp'];
+console.log(arr[1]);
+// alert('Hello World');
+let answer = prompt('ЕСТЬ ЛИ ВАМ 18 ?', 'да');
+console.log(typeof(answer));
+console.log(typeof(arr));
