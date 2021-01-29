4039dbad002421cf155237140bf900e2cec83413
Sviatoslav <sviatoslav29041967@gmail.com>
2021-01-29 18:27:12 +0200

    practic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
  " use strict ";
+let numberOfFilms = prompt("Сколько фильмов Вы посмотрели ?", "");
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+let a = prompt("Один из последних просмотренных Вами фильмов ?", "");
+let b = prompt("На сколько оцените его ?", "");
+let c = prompt("Один из последних просмотренных Вами фильмов ?", "");
+let d = prompt("На сколько оцените его ?", "");
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+console.log(personalMovieDB);
