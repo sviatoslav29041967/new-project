7985a8e11121331ee2fba302f0b79e31fe8e18f2
Sviatoslav <sviatoslav29041967@gmail.com>
2021-02-01 21:37:01 +0200

    Practice part 1

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 " use strict ";
-const num = 50;
- switch (num) {
-  case 49: 
-    console.log('no good');
-    break;
-  case 94:
-    console.log('no');
-    break;
-  case 50:
-    console.log('nice');
-    break;
-  case 111:
-    console.log('No way');
-    break;  
-
- }
