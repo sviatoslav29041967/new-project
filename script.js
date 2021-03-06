aa6456d4aecc6e12c443d3bc80e21b357640a98a
Sviatoslav <sviatoslav29041967@gmail.com>
2021-03-06 14:11:39 +0200

    practice part 3

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
-" use strict ";
+
+ 
+ " use strict ";
+//  function makeWorker() {
+//    let names = "Pete";
+//    return function() {
+//      alert (names);
+//    };
+//  }
+// let names = "John";
+
+//  let work = makeWorker();
+
+//  work();
+
+ //let num = 50;
+
+  /*function showFirstMessage(text) {
+   console.log(text);
+
+   console.log(num);
+ }
+
+ showFirstMessage('hello world');
+   console.log(num);
+   function calc(a, b){
+     return (a + b);
+   }
+   console.log(calc(2, 3));
+   console.log(calc(4, 5));
+   console.log(calc(6, 7));
+   console.log(calc(8, 9));
+
+
+   function ret() {
+     let num = 50;
+     return num;
+   }
+   const anotherNum = ret();
+   console.log(anotherNum);
+
+
+   const logger = function(){
+    console.log('HELLO');
+   };
+   logger();
+   
+
+   const cul = (a, b) => {return a * b;};
+   console.log(cul(4, 8));
+
+   function createCouner() {
+     let counter = 0;
+     const myFunction = function() {
+      counter = counter + 1;
+      return counter;
+     };
+     return myFunction;
+   }
+   const increment = createCouner ();
+   const c1 = increment();
+   const c2 = increment();
+   const c3 = increment();
+   const c4 = increment();
+   const c5 = increment();
+   console.log('example increment', c1, c2, c3, c4,c5 );
+   */
+
+   const str = 'TeXt';
+   console.log(str.toUpperCase());
+   console.log(str);
+   console.log(str.toLowerCase());
+
+   const fruit = 'Some fruit';
+   console.log(fruit.indexOf('fruit'));
+   console.log(fruit.indexOf('Q'));
+
+
+   const logg = 'Hello World';
+   console.log(logg.slice(6, 11));
+
+   console.log(logg.substring(0, 5));
+   console.log(logg.substr(6, 6));
+   
+
+
+   const num = 12.2345;
+   console.log(Math.round(num));
+
+   const test = '352.22px';
+   console.log(parseInt(test));
+   console.log(parseFloat(test));
--- script2.js
+++ script2.js
@@ -1,19 +1,83 @@
 " use strict ";
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели ?', '');
+let numberOfFilms;
+
 const personalMovieDB = {
-  count : numberOfFilms,
-  movies : {},
-  actors : {},
-  genres : [],
-  privat : false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
-const a = prompt ('Один из просмотренных фильмов ?', ''),
-      b = prompt ('На сколько оцените его ?', ''),
-      c = prompt ('Один из просмотренных фильмов ?', ''),
-      d = prompt ('На сколько оцените его ?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log (personalMovieDB);
+
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели ?', '');
+  while (numberOfFilms == "" || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели ?', '');
+
+  }
+}
+
+start();
+
+
+
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотренных фильмов ?', ''),
+      b = prompt('На сколько оцените его ?', '');
+  
+  
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('ERROR');
+      i--;
+    }
+  
+  }
+  
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel(){
+
+if (personalMovieDB.count < 10) {
+  console.log('мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('good');
+} else if (personalMovieDB.count >= 30) {
+  console.log('nice');
+
+} else {
+  console.log("ERROR");
+}
+
+}
+
+detectPersonalLevel();
+
+function showMyDB(){
+
+if (personalMovieDB. privat == false) {
+console.log( personalMovieDB );
+}
+
+}
+
+function writeYouGenres() {
+for (let i = 1; i <= 3; i++) {
+
+personalMovieDB.genres[i - 1] = prompt( `Ваш любимый жанр под номером ${i}` );
+}
+
+}
+
+console.log(personalMovieDB);
